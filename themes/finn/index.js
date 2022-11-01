4178635f2eaf7f300cc38926e764ccd8766dba22
Dave Honneffer <pearofducks@gmail.com>
2022-11-01 13:30:46 +0100

    Yup

--- themes/finn/index.js
+++ themes/finn/index.js
@@ -1,2 +1,11 @@
-export * from './colors.js'
-export * as tokens from './tokens.js'
+// export * from './colors.js'
+// export * as tokens from './tokens.js'
+import fs from 'node:fs'
+import path from 'node:path'
+import yaml from 'yaml'
+import drnm from 'drnm'
+
+const __dirname = drnm(import.meta.url)
+
+export const colors = yaml.parse(fs.readFileSync(path.join(__dirname, './colors.yaml'), 'utf-8'))
+export const tokens = yaml.parse(fs.readFileSync(path.join(__dirname, './tokens.yaml'), 'utf-8'))
