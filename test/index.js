77bd114f68c65c3c0bf22db5935200c337ed35f3
Dave Honneffer <pearofducks@gmail.com>
2022-12-14 21:41:48 +0100

    test

--- test/index.js
+++ test/index.js
@@ -1,6 +1,8 @@
 import { suite } from 'uvu'
 import * as assert from 'uvu/assert'
-import * as util from './src/util.js'
+import * as util from '../src/util.js'
+import path from 'node:path'
+import drnm from 'drnm'
 
 const utilTest = suite('utils')
 
@@ -47,6 +49,17 @@ utilTest('mergeTree underscore ignored', () => {
 utilTest('toCSS', () => {
   assert.is(util.toCSSMap(['foo', 'bar']), '--x-foo: var(--x-bar);')
   assert.is(util.toCSSDef(['foo', 'bar']), '--x-foo: bar;')
+})
+
+utilTest('read yaml', () => {
+  const __dirname = drnm(import.meta.url)
+  const json = util.readYaml(path.join(__dirname, './fixture.yaml'))
+  assert.equal(json, {
+    "hello": "world",
+    "nested": {
+      "object": "here"
+    }
+  })
 })
 
 utilTest.run()
