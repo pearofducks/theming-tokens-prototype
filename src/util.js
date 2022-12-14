3dcaa64c46312692cb7441e84afd6f892f0f2971
Dave Honneffer <pearofducks@gmail.com>
2022-12-14 22:19:14 +0100

    Type

--- src/util.js
+++ src/util.js
@@ -2,17 +2,33 @@ import fs from 'node:fs'
 import yaml from 'yaml'
 import slugify from './slugify.js'
 
+/**
+ * @arg {string} filePath
+ * @returns {object}
+ */
 export const readYaml = filePath => yaml.parse(fs.readFileSync(filePath, 'utf-8'))
 
-const buildToken = (prefix, name) => {
-  const result = []
+/**
+ * Handles merging prefix with name
+ * @arg {string} prefix
+ * @arg {string} name
+ * @returns {string}
+ */
+const buildToken = (prefix, name, result = []) => {
   if (prefix) result.push(prefix)
   if (name !== '_') result.push(name)
   return slugify(result.join(' '))
 }
 
-export const mergeTree = (innerObj, _prefix = '', result = {}) => {
-  for (const [_name, entry] of Object.entries(innerObj)) {
+/**
+ * Merges parent keys onto children - will produce a flat object structure
+ * @arg {Object.<string, string|object>} obj
+ * @arg {string} _prefix
+ * @arg {Object.<string, string>} result
+ * @returns {Object.<string, string>}
+ */
+export const mergeTree = (obj, _prefix = '', result = {}) => {
+  for (const [_name, entry] of Object.entries(obj)) {
     const token = buildToken(_prefix, _name)
     if (typeof entry === 'object') mergeTree(entry, token, result)
     else result[token] = entry
