a59305d42b6e042b62d45993f5cd37467b41b9e7
Dave Honneffer <pearofducks@gmail.com>
2023-01-10 13:44:54 +0100

    Do it

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
 import { processFile } from './src/process.js'
 import { wrapDarkMedia, rootWrap } from './src/util.js'
 import * as lightning from 'lightningcss'
-import fs from 'node:fs'
 import path from 'node:path'
 import glob from 'glob'
 
-const themeLocation = process.argv[2]
-if (!themeLocation) throw 'Need a theme location to process'
-const realPath = path.resolve(themeLocation)
-if (!fs.existsSync(realPath)) throw `${realPath} doesn't seem to exist`
+export default function (folderPath, options) {
+  const CWD = process.cwd()
+  const realPath = path.join(CWD, folderPath)
+  const inputs = glob.sync(`${realPath}/**/*.y?(a)ml`)
+  if (!inputs.length) throw `Nothing found at ${realPath} with the suffix .yml or .yaml`
 
-const inputs = glob.sync(`${realPath}/**/*.y?(a)ml`)
-if (!inputs.length) throw `Nothing found at ${realPath} with the suffix .yml or .yaml`
+  const tokens = inputs.map(processFile)
+  const darkTokens = tokens.filter(t => t.dark).flatMap(t => t.tokens)
+  const normalTokens = tokens.filter(t => !t.dark).flatMap(t => t.tokens)
+  const result = rootWrap(normalTokens.join('\n')) + '\n' + wrapDarkMedia(rootWrap(darkTokens.join('\n')))
 
-const tokens = inputs.map(processFile)
-const darkTokens = tokens.filter(t => t.dark).flatMap(t => t.tokens)
-const normalTokens = tokens.filter(t => !t.dark).flatMap(t => t.tokens)
-const result = rootWrap(normalTokens.join('\n')) + '\n' + wrapDarkMedia(rootWrap(darkTokens.join('\n')))
+  const { code } = lightning.transform({
+    code: Buffer.from(result),
+    minify: options.minify ?? true,
+    targets: {
+      safari: (12 << 16),
+      chrome: (80 << 16),
+      firefox: (80 << 16),
+    }
+  })
 
-const { code } = lightning.transform({
-  code: Buffer.from(result),
-  minify: false,
-  targets: {
-    safari: (12 << 16),
-    chrome: (80 << 16),
-    firefox: (80 << 16),
-  }
-})
-
-console.log("MINIFIED")
-console.log(code.toString())
+  return code
+}
